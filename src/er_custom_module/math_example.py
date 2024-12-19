4a90e15dcdca6f5553496df326f4b8c248211773
Ezequiel Rivero <ezequielmrivero@gmail.com>
2024-12-19 16:01:03 +0100

    first commit example lib

---
+++ src/er_custom_module/math_example.py
@@ -0,0 +1,8 @@
+def add_numbers(a: int, b: int) -> int:
+    """Add two numbers and return the result."""
+    return a + b
+
+
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiply two numbers and return the result."""
+    return a * b
