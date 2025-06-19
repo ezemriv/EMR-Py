e3c05de10373f633a4fdc071e34cc618ce5813ea
Ezequiel Rivero <163421526+ezemriv@users.noreply.github.com>
2025-06-19 14:14:17 +0200

    added get_root_path really utility function

--- src/emrpy/__init__.py
+++ src/emrpy/__init__.py
@@ -1,4 +1,5 @@
 # src/emrpy/__init__.py
 from .telegrambot import TelegramTradingBot
+from .really_utils import get_root_path
 
-__all__ = ["TelegramTradingBot"]
+__all__ = ["TelegramTradingBot", "get_root_path"]
