e322f4a7df7879ce859af66511a43fb4d4488296
Ezequiel Rivero <163421526+ezemriv@users.noreply.github.com>
2025-08-01 12:04:45 +0000

    working on viz

--- src/emrpy/visualization/__init__.py
+++ src/emrpy/visualization/__init__.py
@@ -1,4 +1,4 @@
 # src/emrpy/visualization/__init__.py
-from .timeseries import plot_timeseries, plot_multiple_log_returns
+from .timeseries import plot_timeseries
 
-__all__ = ["plot_timeseries", "plot_multiple_log_returns"]
+__all__ = ["plot_timeseries"]
